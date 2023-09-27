a83af40ac717692ab2b4ffa70a8b0a81b4eaed90
evankeane <145344743+evankeane@users.noreply.github.com>
2023-09-27 14:46:30 +0700

    Update Jurnal01.java
    first commit

--- Jurnal01.java
+++ Jurnal01.java
@@ -2,7 +2,6 @@ public class Jurnal01 {
     public static void main(String[] args) {
         String MyName = "Abraham Evan Keane Nelwan";
         String MyNIM = "607062300068D3";
-        String MyClass = " RPLA Kelas 01";
         String MyClass = " D3 RPLA 47-01";
         
         System.out.println("Name: " + MyName);
