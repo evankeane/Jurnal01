4e6c997620b335fd834c2669cf498d8c5379022d
evankeane <145344743+evankeane@users.noreply.github.com>
2023-09-27 14:03:46 +0700

    Create Jurnal01.java
    first commit

---
+++ Jurnal01.java
@@ -0,0 +1,15 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        String MyName = "Abraham Evan Keane Nelwan";
+        String MyNIM = "607062300068D3";
+        String MyClass = " RPLA Kelas 01";
+        
+        System.out.println("Name: " + MyName);
+        System.out.println("NIM: " + MyNIM);
+        System.out.println("Class: " + MyClass);
+      
+
+
+
+    }
+}
